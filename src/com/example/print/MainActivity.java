0be551262a06106481ed43cfd935d6e77792b425
linyijuan <linyijuan22@gmail.com>
2016-05-10 18:30:05 +0800

    d

--- src/com/example/print/MainActivity.java
+++ src/com/example/print/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.print;
 
+// transimission usage 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,111 +11,153 @@ import java.io.PrintWriter;
 import java.io.StringBufferInputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+// data structure and widget 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+// Android widget, activity and view
+import android.widget.TextView;
 import android.R.bool;
 import android.app.Activity;
 import android.content.Context;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.util.Log;
-import android.widget.TextView;
 
+// Android wifi usage
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+
+// Android log 
+import android.util.Log;
+
+/* SMS listener: MessageListener class */
 import com.example.print.SMSBroadcastReceiver.MessageListener;
 
-
+/* Activity that detects free printer wifi and deliver printing jobs 
+that are received from SMS */
 public class MainActivity extends Activity{
-	
+
+	// Android wifi usage
 	private WifiManager wifiManager;
 	private List<ScanResult> list;
 	private List<WifiConfiguration> ConfList;
 	private SimpleDateFormat sDateFormat;
-	private TextView textView;
-	private TextView textView1;
 	private ArrayList<String> ssidList = new ArrayList<String>();
 	private ArrayList<String> bssidList = new ArrayList<String>();
+	
+	// Android widget and view
+	private TextView textView;
+	private TextView textView1;
+
+	/* SMS listener: MessageListener class */
+	private SMSBroadcastReceiver mSMSBroadcastReceiver;
+
+	// global container
+	private String sms = "";
 	private String companyName;
 	private String printerIpAddres = "192.168.223.1";
+
+	// global flag
+	private boolean firstTime = true;
 	private boolean isPrinter = false;
 	private boolean isPrinted = false;
 	private boolean reset_signal = false;
-	private SMSBroadcastReceiver mSMSBroadcastReceiver;
-	private String sms = "";
-	private boolean firstTime = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        sDateFormat =  new SimpleDateFormat("MM-dd hh:mm:ss");
 
+        /*********************
+        **** preparation *****
+        **********************/
+
+        // date formatter
+        sDateFormat =  new SimpleDateFormat("MM-dd hh:mm:ss");
+        // find views
         textView = (TextView) findViewById(R.id.textview);
         textView1 = (TextView) findViewById(R.id.textview1);
-        
+
+        // instantiate MessageListener class
         mSMSBroadcastReceiver = new SMSBroadcastReceiver();
-		mSMSBroadcastReceiver.setOnReceivedMessageListener(new MessageListener() {
-					@Override
-					public void OnReceived(String message) {
-						reset_signal = true;
-						sms = message;
-					}
+        // store corresponding information and signal on receiving SMS
+		mSMSBroadcastReceiver.setOnReceivedMessageListener( new MessageListener() {
+			@Override
+			public void OnReceived(String message) {
+				reset_signal = true;
+				sms = message;
+			}
 		});
         
-        
+        /*********************
+        *** start detection **
+        **********************/
+
+        // THREAD: loop to check availability of free printer wifi
         final Handler handler = new Handler();
         Runnable runnable = new Runnable() {
 			
 			@Override
 			public void run() {
+				// clear the container that holds the ssid of all WIFIs
 				ssidList.clear();
+				// check if there is a detection of new free printer
 				isPrinter = isPrinter();
 				textView.setText(isPrinter+"");
+				// once new free printer wifi detected 
 				if(isPrinter){
+					// conventionally retrieve company name as the longest common substring of all wifi ssid
 					companyName = longestCommonSubstringInList(ssidList);
 					textView1.setText(companyName);
-					Log.i("cpppp", "print job here");
+					// send printing task to desired target
 					printDocument(companyName);					
 				}
 				handler.post(this);
 			}
 		};
-		
+		// start looping at an interval of 3 sec
 		handler.postDelayed(runnable, 3000);
 
     }
     
-    /*To check if there's a new printer */
-    
+    /* Method to check if there is a detection of new free printer */
 	private boolean isPrinter(){
     	boolean is = false;
+    	// instantiate Android wifiManager
     	wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    	// obtain list of wifi information and network configuration 
 		list = wifiManager.getScanResults();
 		ConfList = wifiManager.getConfiguredNetworks();
-		if (list == null) {
-			return is;
-		}else {
+
+		// perform below if any wifi is scanned
+		if (list != null){
+			// check the availability of scanned wifi one by one
 			for (int i = 0; i<list.size();i++){
+
+				//retrieve the ssid and bssid of certain scanned wifi
 				ScanResult scanResult = list.get(i);
 				String ssid = scanResult.SSID;
-//				String ssid = ConfList.get(i).SSID;
 				String bssid = scanResult.BSSID;
-//				String bssid = ConfList.get(i).BSSID;
-				
 				String cap = scanResult.capabilities;
 				ssidList.add(ssid);
-				if((ssid.toLowerCase().contains("print")||ssid.toLowerCase().contains("hp")||ssid.toLowerCase().contains("canon"))&&(!cap.contains("WPA"))){
+
+				//check if certain wifi is within our interest
+				if((ssid.toLowerCase().contains("print")||
+					ssid.toLowerCase().contains("hp")||
+					ssid.toLowerCase().contains("canon"))
+					&&
+					(!cap.contains("WPA"))){
+					// reset everything if signaled
 					if (reset_signal){
 						bssidList = new ArrayList<String>();
 						reset_signal = false;
 					}
+					// add bssid to container if the wifi is of our interest
 					if(!bssidList.contains(bssid)){
 						bssidList.add(bssid);					
 //						wifiManager.disconnect();	
@@ -127,19 +170,20 @@ public class MainActivity extends Activity{
 //							}
 //								
 //						}					
-//						wifiManager.reconnect();				
+//						wifiManager.reconnect();	
+						// notify detection of free printer			
 						return true;
 					}
 				}
 			}
 		}
+		// notify no detection of free printer	
 		return false;
     }
 	
-    
+    /* Snippet to find longest common substring in an Arraylist */
 	private static String longestCommonSubstringInList(ArrayList<String> values)
 	{
-		//System.out.println(values.size());
 	    String result = "";
 	    int largest = 0;
 	    
@@ -150,13 +194,13 @@ public class MainActivity extends Activity{
 	            String tmp = longestCommonSubstring(values.get(i), values.get(j));
 	            //Log.e("JKLASJ", tmp);
 	            tmp = trim(tmp);
-	            if (tmp!=null){
+	            if (tmp != null){
 	            	if(!commonStringList.contains(tmp)){
 	            		commonStringList.add(tmp);
 	            		countList.add(1);
 	            	} else {
 	            		int index = commonStringList.indexOf(tmp);
-	            		countList.set(index,countList.get(index)+1);
+	            		countList.set(index,countList.get(index) + 1 );
 	            	}
 	            }
 	        }
@@ -164,17 +208,18 @@ public class MainActivity extends Activity{
 	    
 	    int current;
 
-	    for(int i=0;i<commonStringList.size();i++){
+	    for( int i=0; i<commonStringList.size(); i++){
 	    	current = countList.get(i);
-	    	if(current>largest){
+	    	if(current > largest){
 	    		largest = current;
-	    		result= commonStringList.get(i);
+	    		result = commonStringList.get(i);
 	    	}
 	    }	
 	    return result;
 
 	}
     
+    /* Snippet to find longest common substring */
 	private static String longestCommonSubstring(String S1, String S2)
 	{
 	    int Start = 0;
@@ -195,53 +240,64 @@ public class MainActivity extends Activity{
 	    return S1.substring(Start, (Start + Max));
 	}
 	
+	/* Method to remove unwanted character in a string */
 	private static String trim(String s){
 		s = s.replaceAll("[^a-zA-Z]", "");
 		return s;
 	}
 	
+	/* Method to get file path*/
 	public void getFile(){
 		File file = new File(Environment.getExternalStorageState());
 	}
 	
+	/* Method to execute printing task*/
 	public void printDocument(String s){
 		new PrintTask().execute(new String[]{s});	    
 	}
 	
+	/* Async dlivering printing job to connected printer */
 	class PrintTask extends AsyncTask<String, Void, Void> {
-		protected void onPostExecute() {}
-
 		@Override
 		protected Void doInBackground(String... arg0) {
 			try {
-			Socket sock = new Socket(printerIpAddres, 9100);
-			PrintWriter oStream = new PrintWriter(sock.getOutputStream());
-			
-			OutputStream outputStream = sock.getOutputStream();	   
-			if(sms.contains("drone") | sms.equals("")){
-	    		File myFile =new File("/storage/emulated/0/Download/IMG_7254.jpg");
-				FileInputStream fis= new FileInputStream(myFile);
-				byte[] buffer = new byte[500];
-				int count = 0;
-				
-				while((count=fis.read(buffer,0,buffer.length))!=-1){
-				outputStream.write(buffer,0,count);
+				// configure socket connection
+				Socket sock = new Socket(printerIpAddres, 9100);
+				// prepare stream to printer
+				PrintWriter oStream = new PrintWriter( sock.getOutputStream() );
+				OutputStream outputStream = sock.getOutputStream();
+				// seperate type of printing task: 
+				// 1. printing drone image with information
+				if(sms.contains("drone") | sms.equals("")){
+					// find the file in the path
+					File myFile = new File("/storage/emulated/0/Download/IMG_7254.jpg");
+					// prepare stream and buffer object
+					FileInputStream fis = new FileInputStream(myFile);
+					byte[] buffer = new byte[500];
+					int count = 0;
+					// deliver bytes of image to printing
+					while((count = fis.read(buffer, 0, buffer.length)) != -1){
+						outputStream.write(buffer, 0, count);
+					}
+					// send it out in output stream
+					outputStream.flush();	
 				}
-				outputStream.flush();	
-	    	}
-	    	else {
-				oStream.println("Hi! :) "+arg0[0]+": "+sms);
-				oStream.println("\n\n\n");
-			}	
-
-			oStream.close();
-			sock.close();
-			} catch (UnknownHostException e) {
-			e.printStackTrace();
-			} catch (IOException e) {
-			e.printStackTrace();
+				// 2. printing sms message received
+				else {
+					// deliver bytes of text message to printing
+					oStream.println( "Hi! :) " + arg0[0] + ": " + sms );
+					oStream.println( "\n\n\n" );
+				}
+				// shut down the print writer
+				oStream.close();
+				// close socket connection 
+				sock.close();
+			} catch ( UnknownHostException e ) {
+				e.printStackTrace();
+			} catch ( IOException e ) {
+				e.printStackTrace();
 			}
 			return null;
 		}
-		}
+	}
 }
--- src/com/example/print/SMSBroadcastReceiver.java
+++ src/com/example/print/SMSBroadcastReceiver.java
@@ -1,50 +1,60 @@
 package com.example.print;
 
+// java date formatter
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+// android sms helper
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsMessage;
 
+/* Class that inherits Broadcast Receiver */
 public class SMSBroadcastReceiver extends BroadcastReceiver {
-    private static MessageListener mMessageListener;
+	// inherit BroadcastReceiver constructer 
 	public SMSBroadcastReceiver() {
 		super();
 	}
-
+	// override the method invoked when sms is received
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		     Object [] pdus= (Object[]) intent.getExtras().get("pdus");
-		     for(Object pdu:pdus){
-		    	SmsMessage smsMessage=SmsMessage.createFromPdu((byte [])pdu);
-		    	String sender=smsMessage.getDisplayOriginatingAddress();
-		    	String content=smsMessage.getMessageBody();
-		    	long date=smsMessage.getTimestampMillis();
-		    	Date timeDate=new Date(date);
-		    	SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		    	String time=simpleDateFormat.format(timeDate);
-		    	
-		    	System.out.println("sms from:"+sender);
-		    	System.out.println("sms content:"+content);
-		    	System.out.println("sms time:"+time);
-		    	
-		    	mMessageListener.OnReceived(content);
-		    	
-		    	
-		    	if("5556".equals(sender)){
-		    		System.out.println(" abort ");
-		    		abortBroadcast();
-		    	}
-		    	
-		     }
-	}
-	
-		public interface MessageListener {
-			public void OnReceived(String message);
-		}
+		// obtain list of "pdus"
+		Object [] pdus= (Object[]) intent.getExtras().get("pdus");
+		for(Object pdu: pdus){
+			// retrieve message and related information
+			SmsMessage smsMessage = SmsMessage.createFromPdu((byte [])pdu);
+			String sender = smsMessage.getDisplayOriginatingAddress();
+			String content = smsMessage.getMessageBody();
+			long date = smsMessage.getTimestampMillis();
+			Date timeDate = new Date(date);
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String time = simpleDateFormat.format(timeDate);
 
-		public void setOnReceivedMessageListener(MessageListener messageListener) {
-			this.mMessageListener=messageListener;
+			// debug use
+			System.out.println("sms from:"+sender);
+			System.out.println("sms content:"+content);
+			System.out.println("sms time:"+time);
+
+			// notify onReceived method in message listener 
+			mMessageListener.OnReceived(content);
+
+			// abort broadcast
+			if("5556".equals(sender)){
+				System.out.println(" abort ");
+				abortBroadcast();
+			}
 		}
+	}
+
+	// static message listener
+    private static MessageListener mMessageListener;
+	// interface: MessageListener that requires override of onReceive method
+	public interface MessageListener {
+		public void OnReceived(String message);
+	}
+	// setter of message listener
+	public void setOnReceivedMessageListener(MessageListener messageListener) {
+		this.mMessageListener = messageListener;
+	}
 }
